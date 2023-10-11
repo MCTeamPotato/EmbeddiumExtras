ca3e960bcc41d0758117ed9e29a58c242ed47713
Kasualix <670067575@qq.com>
2023-10-11 11:36:27 +0800

    fix EntityRendererManager NPE

--- src/main/java/vice/magnesium_extras/mixins/entitydistance/MaxDistanceEntity.java
+++ src/main/java/vice/magnesium_extras/mixins/entitydistance/MaxDistanceEntity.java
@@ -3,6 +3,7 @@ package vice.magnesium_extras.mixins.entitydistance;
 import net.minecraft.client.renderer.culling.ClippingHelper;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,17 +13,16 @@ import vice.magnesium_extras.config.EntityListConfig;
 import vice.magnesium_extras.config.MagnesiumExtrasConfig;
 import vice.magnesium_extras.util.DistanceUtility;
 
-import java.util.Objects;
-
 @Mixin(EntityRendererManager.class)
 public abstract class MaxDistanceEntity {
     @Inject(at = @At("RETURN"), method = "shouldRender", cancellable = true)
     public <E extends Entity> void shouldDoRender(E entity, ClippingHelper clippingHelper, double cameraX, double cameraY, double cameraZ, @NotNull CallbackInfoReturnable<Boolean> cir) {
-        if (!cir.getReturnValue()) return;
+        ResourceLocation id = entity.getType().getRegistryName();
+        if (!cir.getReturnValue() || id == null) return;
         if (
                 !MagnesiumExtrasConfig.enableDistanceChecks.get() ||
-                EntityListConfig.ENTITY_LIST.get().contains(Objects.requireNonNull(entity.getType().getRegistryName()).toString()) ||
-                EntityListConfig.MODID_LIST.get().contains(Objects.requireNonNull(entity.getType().getRegistryName()).getNamespace()) ||
+                EntityListConfig.ENTITY_LIST.get().contains(id.toString()) ||
+                EntityListConfig.MODID_LIST.get().contains(id.getNamespace()) ||
                 DistanceUtility.isEntityWithinDistance(
                         entity,
                         cameraX,
