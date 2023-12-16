c87edbc37b6c4bd09e5d4692d389e1cc934aeb15
Kasualix <670067575@qq.com>
2023-12-16 12:12:19 +0800

    remove gl debug verbosity tweak

--- src/main/java/com/teampotato/embeddiumextras/mixins/options_improvement/GameSettingsMixin.java
+++ src/main/java/com/teampotato/embeddiumextras/mixins/options_improvement/GameSettingsMixin.java
@@ -7,11 +7,6 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(GameSettings.class)
 public abstract class GameSettingsMixin {
-    @Redirect(method = "load", at = @At(value = "INVOKE", target = "Ljava/lang/Integer;parseInt(Ljava/lang/String;)I", ordinal = 14))
-    private int disableOpenGLDebugMessage(String s) {
-        return 0;
-    }
-
     @Redirect(method = "load", at = @At(value = "INVOKE", target = "Ljava/lang/String;equals(Ljava/lang/Object;)Z", ordinal = 80))
     private boolean alwaysSkipMultiplayerWarning(String s, Object o) {
         return true;
