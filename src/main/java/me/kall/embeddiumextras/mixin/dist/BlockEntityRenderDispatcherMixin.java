65c1f79ed45c32ae9763e1e3662095887188e0e8
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-10-03 17:01:35 +0800

    fix

--- src/main/java/me/kall/embeddiumextras/mixin/dist/BlockEntityRenderDispatcherMixin.java
+++ src/main/java/me/kall/embeddiumextras/mixin/dist/BlockEntityRenderDispatcherMixin.java
@@ -1,20 +1,21 @@
 package me.kall.embeddiumextras.mixin.dist;
 
-import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.mojang.blaze3d.vertex.PoseStack;
 import me.kall.embeddiumextras.config.ExtrasConfig;
 import me.kall.embeddiumextras.features.dist.Renderable;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderDispatcher;
-import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BlockEntityRenderDispatcher.class)
 public abstract class BlockEntityRenderDispatcherMixin {
-    @WrapMethod(method = "setupAndRender")
-    private static <T extends BlockEntity> void onRender(BlockEntityRenderer<T> renderer, T blockEntity, float partialTicks, PoseStack matrixStack, MultiBufferSource buffer, Operation<Void> original) {
-        if (ExtrasConfig.ENABLE_TILE_ENTITY_DIST_CHECK.get() || ((Renderable)blockEntity.getType()).dist$alwaysRenderable() || ((Renderable.Entity)blockEntity).dist$renderable()) original.call(renderer, blockEntity, partialTicks, matrixStack, buffer);
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/entity/BlockEntity;getViewDistance()D", shift = At.Shift.AFTER), cancellable = true)
+    private <E extends BlockEntity> void after(E blockEntity, float partialTicks, PoseStack matrixStack, MultiBufferSource buffer, CallbackInfo ci) {
+        if (ExtrasConfig.ENABLE_TILE_ENTITY_DIST_CHECK.get() || ((Renderable)blockEntity.getType()).dist$alwaysRenderable() || ((Renderable.Entity)blockEntity).dist$renderable()) return;
+        ci.cancel();
     }
 }
--- src/main/java/me/kall/embeddiumextras/mixin/dist/EntityRenderDispatcherMixin.java
+++ src/main/java/me/kall/embeddiumextras/mixin/dist/EntityRenderDispatcherMixin.java
@@ -1,18 +1,21 @@
 package me.kall.embeddiumextras.mixin.dist;
 
-import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import me.kall.embeddiumextras.config.ExtrasConfig;
 import me.kall.embeddiumextras.features.dist.Renderable;
 import net.minecraft.client.renderer.culling.Frustum;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(EntityRenderDispatcher.class)
 public abstract class EntityRenderDispatcherMixin {
-    @WrapMethod(method = "shouldRender")
-    private <E extends Entity> boolean onCheck(E entity, Frustum frustum, double camX, double camY, double camZ, Operation<Boolean> original) {
-        return (((Renderable)entity.getType()).dist$alwaysRenderable() || !ExtrasConfig.ENABLE_ENTITY_DIST_CHECK.get() || ((Renderable.Entity)entity).dist$renderable()) ? original.call(entity, frustum, camX, camY, camZ) : false;
+    @Inject(method = "shouldRender", at = @At("RETURN"), cancellable = true)
+    private <E extends Entity> void onRenderCheck(E entity, Frustum frustum, double camX, double camY, double camZ, CallbackInfoReturnable<Boolean> cir) {
+        if (!cir.getReturnValue()) return;
+        if ((((Renderable)entity.getType()).dist$alwaysRenderable() || !ExtrasConfig.ENABLE_ENTITY_DIST_CHECK.get() || ((Renderable.Entity)entity).dist$renderable())) return;
+        cir.setReturnValue(false);
     }
 }
